7c0a5275ff27dc86f4774b5cdd7efbef67b6eede
Wesley Nogueira <wesleysena.dev@gmail.com>
2023-06-24 22:54:42 -0300

    Test: Adiciona verificação de callback na factory de ser

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,13 +1,19 @@
 FactoryBot.define do
   factory :user do
-    email            { Faker::Internet.email(domain: 'gmail.com') }
+    email            { Faker::Internet.email(name: 'test', domain: 'test.com') }
     password         { Faker::Internet.password }
     confirm_password { password }
 
     trait :skip_add_profile_callback do
       after(:build) do |user|
-        User.skip_callback(:create, :after, :add_profile)
+        if callback_exists?(:add_profile)
+          User.skip_callback(:create, :after, :add_profile)
+        end
       end
     end
   end
+end
+
+def callback_exists?(callback)
+  User._create_callbacks.any? { |callbacks| callbacks.filter == callback }
 end
